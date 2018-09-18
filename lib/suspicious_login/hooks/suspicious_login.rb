abf0fdf5b063a85a0ab43e0d6b81c4126a9bd447
Colin Browne <ceres629@gmail.com>
2018-09-18 13:47:23 +0100

    use devise invalid message by default on suspicious login

--- lib/suspicious_login/hooks/suspicious_login.rb
+++ lib/suspicious_login/hooks/suspicious_login.rb
@@ -3,6 +3,6 @@ Warden::Manager.after_set_user except: :fetch do |user, warden, opts|
     user.send_suspicious_login_instructions if user.login_token_sent_at.nil? || Time.now.utc - user.login_token_sent_at > Devise.resend_token_after
     scope = opts[:scope]
     warden.logout(scope) if warden.authenticated?(scope)
-    throw(:warden, message: :suspicious_login)
+    throw(:warden, message: I18n.t('devise.failure.invalid'))
   end
 end
