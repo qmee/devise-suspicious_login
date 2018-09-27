1e05b375dcd04e8512d43e3838fecddaf917dc89
Colin Browne <ceres629@gmail.com>
2018-09-27 18:49:47 +0100

    file name change

--- lib/generators/suspicious_login/install_generator.rb
+++ lib/generators/suspicious_login/install_generator.rb
@@ -1,9 +1,9 @@
 module SuspiciousLogin
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
-      desc "Add SuspiciousLogin config variables to the Devise initializer"
 
+      source_root File.expand_path("../../templates", __FILE__)
+      desc "Add SuspiciousLogin config variables and files to rails project"
       def create_initializer
         template('suspicious_login.rb', 'config/initializers/suspicious_login.rb')
       end
