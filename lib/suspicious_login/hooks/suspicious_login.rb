adbc8186ce70cd328eaced686aecfcd5d94ed7b8
Colin Browne <ceres629@gmail.com>
2018-10-24 10:23:43 +0100

    refactor

--- lib/suspicious_login/hooks/suspicious_login.rb
+++ lib/suspicious_login/hooks/suspicious_login.rb
@@ -1,5 +1,5 @@
 Warden::Manager.after_set_user except: :fetch do |user, warden, opts|
-  if user && user.respond_to?(:suspicious?) && user.suspicious?(warden.request) && !user.token_login?
+  if user && !user.token_login? && user.respond_to?(:suspicious?) && user.suspicious?(warden.request)
     user.send_suspicious_login_instructions(warden.request) if user.login_token_sent_at.nil? || Time.now.utc - user.login_token_sent_at > Devise.resend_login_token_after
     scope = opts[:scope]
     warden.logout(scope) if warden.authenticated?(scope)
