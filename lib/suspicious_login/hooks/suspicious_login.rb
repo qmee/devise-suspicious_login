7f28bfcff7c4a704793050619581874984b8849b
Colin Browne <ceres629@gmail.com>
2018-10-31 14:57:58 +0000

    only trigger hook on DatabaseAuthenticatable login

--- lib/suspicious_login/hooks/suspicious_login.rb
+++ lib/suspicious_login/hooks/suspicious_login.rb
@@ -1,5 +1,5 @@
 Warden::Manager.after_set_user except: :fetch do |user, warden, opts|
-  if user && !user.token_login? && user.respond_to?(:suspicious?) && user.suspicious?(warden.request)
+  if warden.winning_strategy.class == Devise::Strategies::DatabaseAuthenticatable && user && !user.token_login? && user.respond_to?(:suspicious?) && user.suspicious?(warden.request)
     user.send_suspicious_login_instructions(warden.request) if user.login_token_sent_at.nil? || Time.now.utc - user.login_token_sent_at > Devise.resend_login_token_after
     scope = opts[:scope]
     warden.logout(scope) if warden.authenticated?(scope)
